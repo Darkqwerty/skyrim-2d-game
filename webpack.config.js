53616d7765da13bd1ca8901f26cb05af84031a67
Darkqwerty <darkqwerty.2.0@gmail.com>
2021-06-28 21:57:44 +0300

    Homework-1 fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,61 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
+// eslint-disable-next-line prefer-destructuring
 const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = {
-  mode: NODE_ENV || 'development',
-  entry: path.resolve(__dirname, './src/index.js'),
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  watch: NODE_ENV === 'development',
-  watchOptions: {
-    ignored: /node_modules/,
-    poll: 1000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: [/node_modules/],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: [/\.svg$/, /\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        use: [
+    mode: NODE_ENV || 'development',
+    entry: path.resolve(__dirname, './src/index.js'),
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    watch: NODE_ENV === 'development',
+    watchOptions: {
+        ignored: /node_modules/,
+        poll: 1000,
+    },
+    module: {
+        rules: [
             {
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]',
-                    outputPath: 'assets/',
-                }
-            }
-        ]
-      }
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: [/node_modules/],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: [/\.svg$/, /\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+        }),
     ],
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html'),
-    }),
-  ],
-  devServer: {
-    port: 3000,
-    overlay: true,
-    open: true,
-    hot: true,
-    historyApiFallback: true,
-  },
-  devTool: NODE_ENV === 'development' ? 'source-map' : false,
+    devServer: {
+        port: 3000,
+        overlay: true,
+        open: true,
+        hot: true,
+        historyApiFallback: true,
+    },
+    devTool: NODE_ENV === 'development' ? 'source-map' : false,
 };
