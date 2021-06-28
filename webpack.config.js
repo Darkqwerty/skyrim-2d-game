1c383dd188e7b292f8233d00c0e80fd20b186889
Darkqwerty <darkqwerty.2.0@gmail.com>
2021-06-28 22:25:25 +0300

    Homework-1 fix fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
@@ -57,5 +58,5 @@ module.exports = {
         hot: true,
         historyApiFallback: true,
     },
-    devTool: NODE_ENV === 'development' ? 'source-map' : false,
+    devtool: NODE_ENV === 'development' ? 'source-map' : false,
 };
