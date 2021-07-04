a6138ff031386f1bd6009558c900b5ff8c6bfb2b
Darkqwerty <darkqwerty.2.0@gmail.com>
2021-07-04 14:41:49 +0300

    Homework-1

---
+++ src/mapSprites.js
@@ -0,0 +1,29 @@
+export const mapSprite = {
+    grass: [
+        {
+            access: true,
+            x: 0,
+            y: 16 * 6,
+        },
+        {
+            access: true,
+            x: 16,
+            y: 16 * 6,
+        },
+        {
+            access: true,
+            x: 16 * 1,
+            y: 16 * 6,
+        },
+        {
+            access: true,
+            x: 16 * 0,
+            y: 16 * 7,
+        },
+    ],
+    sand: {
+        access: true,
+        x: 16 * 3,
+        y: 16 * 6,
+    },
+};
