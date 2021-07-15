7827cc178d230637f9f18496808a46225f1a3aac
Darkqwerty <darkqwerty.2.0@gmail.com>
2021-07-15 21:09:45 +0300

    Homework-3

--- src/Client/ClientWorld.js
+++ src/Client/ClientWorld.js
@@ -1,32 +1,59 @@
-import { spriteH, spriteW } from './../const';
+import PositionedObject from './../common/PositionedObject';
+import ClientCell from './ClientCell';
 
-class ClientWorld {
+class ClientWorld extends PositionedObject {
     constructor(game, engine, levelCfg) {
+        super();
+
+        const worldHeight = levelCfg.map.length;
+        const worldWidth = levelCfg.map[0].length;
+        const cellSize = engine.canvas.height / levelCfg.camera.height;
+
         Object.assign(this, {
             game,
             engine,
             levelCfg,
-            height: levelCfg.map.lenght,
-            width: levelCfg.map[0].lenght,
-        })
+            height: worldHeight * cellSize,
+            width: worldWidth * cellSize,
+            worldWidth,
+            worldHeight,
+            cellWidth: cellSize,
+            cellHeight: cellSize,
+            map: [],
+        });
     }
 
     init() {
-        this.levelCfg.map.forEach((cfgRow, y) => {
-            cfgRow.forEach((cfgCell, x) => {
-                const spriteGroup = 'terrain';
-                const spriteName = cfgCell[0][0];
+        const { levelCfg, map, worldWidth, worldHeight } = this;
 
-                this.engine.renderSpriteFrame({
-                    sprite: [spriteGroup, spriteName],
-                    frame: 0,
-                    x: x * spriteW,
-                    y: y * spriteH,
-                    w: spriteW,
-                    h: spriteH
-                });
-            });
-        });
+        for (let row = 0; row < worldHeight; row++) {
+            for (let col = 0; col < worldWidth; col++) {
+                if (!map[row]) {
+                    map[row] = [];
+                }
+
+                map[row][col] = new ClientCell({
+                    world: this,
+                    cellCol: col,
+                    cellRow: row,
+                    cellCfg: levelCfg.map[row][col],
+                })
+            }
+        }
+    }
+
+    cellAt(col, row) {
+        return this.map[row] && this.map[row][col];
+    }
+
+    render(time) {
+        const { map, worldWidth, worldHeight } = this;
+
+        for (let row = 0; row < worldHeight; row++) {
+            for (let col = 0; col < worldWidth; col++) {
+                map[row][col].render(time)
+            }
+        }
     }
 }
 
